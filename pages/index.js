6231b13bbfc14e1ed81c0ed42882dd3de079d0a3
Thiago24-cloud <thiagochimezie4@gmail.com>
2025-12-26 12:00:23 -0300

    Update and rename index.js to index.js.

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Dashboard() {
-  return (
-    <div style={{ padding: '20px', textAlign: 'center' }}>
-      <h1>💸 Meu Dashboard Financeiro - FinMemory</h1>
-      <p>O site está funcionando! Próximo passo: Conectar ao Supabase.</p>
-    </div>
-  );
-}
