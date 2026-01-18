7841d0aa38492b2404a63f1b218990738d3f35af
Thiago24-cloud <thiagochimezie4@gmail.com>
2026-01-18 16:00:30 -0300

    remove: exclui pages/login.js pois originalmente não existia

--- pages/login.js
+++ pages/login.js
@@ -1,6 +1,1 @@
-// Backup do login.js de teste criado em 18/01/2026
-// Caso queira restaurar, basta copiar o conteúdo deste arquivo para pages/login.js
 
-// (Arquivo originalmente não existia, então este backup é apenas um placeholder)
-
-// Se futuramente houver um login.js antigo, cole aqui o conteúdo antigo para restauração.
